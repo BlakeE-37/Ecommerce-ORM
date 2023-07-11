b8d47736d9a2820581a42162a66d424affc30445
Blake H <blake.hollingsworth80@gmail.com>
2023-07-11 14:33:16 -0600

    added get route by ID, post route, and put route for the Tag Model

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -14,17 +14,41 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const tagData = await Tag.findByPk(req.params.id, { include: [{ all: true, nested: true }] });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  try {
+    const Data = await Tag.create({
+      tag_name: req.body.tag_name,
+    });
+    res.status(200).json(Data);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const Data = await Tag.update({ tag_name: req.body.tag_name }, {
+      where: {
+        id: req.params.id
+      }
+    });
+    res.status(200).json("Category Updated")
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 router.delete('/:id', (req, res) => {
